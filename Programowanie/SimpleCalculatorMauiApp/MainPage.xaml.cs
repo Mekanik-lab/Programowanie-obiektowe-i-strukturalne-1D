87e28d32e9dc6b7490865f04da8eb5be0eab2c0f
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-15 11:52:29 +0200

    Backend kalkulatora

--- Programowanie/SimpleCalculatorMauiApp/MainPage.xaml.cs
+++ Programowanie/SimpleCalculatorMauiApp/MainPage.xaml.cs
@@ -6,5 +6,18 @@
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object? sender, EventArgs e)
+        {
+           if(int.TryParse(firstNumberEntry.Text, out int firstNumber)
+              && int.TryParse(secondNumberEntry.Text, out int secondNumber))
+            {
+                int result = firstNumber + secondNumber;
+                resultLabel.Text = "Wynik to: " + result;
+            } else
+            {
+                resultLabel.Text = "Podałeś błędne dane";
+            }
+        }
     }
 }
